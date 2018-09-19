f5766fb90b930354898b20a88c2104cc8652826e
unknown <hai6722@gmail.com>
2018-09-19 18:03:50 +0700

    new

--- app/src/main/java/com/example/dtanp/masoi/home.java
+++ app/src/main/java/com/example/dtanp/masoi/home.java
@@ -1,15 +1,13 @@
 package com.example.dtanp.masoi;
 
 import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 
-public class MainActivity extends Activity {
-
+public class home extends Activity {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-
     }
 }
