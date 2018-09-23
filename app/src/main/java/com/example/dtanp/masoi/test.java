575a7203a3954d0fe64727456217dbf801b9002c
unknown <hai6722@gmail.com>
2018-09-24 01:54:32 +0700

    new

--- app/src/main/java/com/example/dtanp/masoi/test.java
+++ app/src/main/java/com/example/dtanp/masoi/test.java
@@ -3,14 +3,11 @@ package com.example.dtanp.masoi;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.widget.ImageButton;
 
-public class tim  extends Activity{
-    ImageButton imguser1;
+public class test extends Activity{
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tim);
-        imguser1=findViewById(R.id.bguser1);
+        setContentView(R.layout.activity_test);
     }
 }
