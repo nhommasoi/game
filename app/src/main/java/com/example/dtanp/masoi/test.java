1e9eaa8eae5fea945e5009778678731cce5187af
unknown <hai6722@gmail.com>
2018-09-24 09:48:11 +0700

    new

--- app/src/main/java/com/example/dtanp/masoi/test.java
+++ app/src/main/java/com/example/dtanp/masoi/test.java
@@ -4,10 +4,8 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 
-public class test extends Activity{
-    @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_test);
-    }
+public class test {
+
+
+
 }
